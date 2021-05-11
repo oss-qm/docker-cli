c6740b9cfe57b9210e6585cdae60929a1baef5a2
Enrico Weigelt, metux IT consult <info@metux.net>
2021-05-11 11:23:11 +0200

    1

--- scripts/docs/generate-man.sh
+++ scripts/docs/generate-man.sh
@@ -7,13 +7,16 @@ GO=${GO:-"go"}
 mkdir -p ./man/man1
 
 if ! command -v go-md2man &> /dev/null; then
+	echo "=== building go-md2man"
 	# yay, go install creates a binary named "v2" ¯\_(ツ)_/¯
 	$GO build -o "/go/bin/go-md2man" ./vendor/github.com/cpuguy83/go-md2man/v2
 fi
 
 # Generate man pages from cobra commands
+echo "=== generate manpages"
 $GO build -o /tmp/gen-manpages github.com/docker/cli/man
 /tmp/gen-manpages --root "$(pwd)" --target "$(pwd)/man/man1"
 
+echo "=== running legacy manpage build"
 # Generate legacy pages from markdown
 ./man/md2man-all.sh -q
